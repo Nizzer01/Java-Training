861172dcfe28a418d9bc0246e5510a26a99d871d
Gav Shields <65550958+Nizzer01@users.noreply.github.com>
2024-06-05 11:12:42 +0100

    Java basics overview

--- IfKeywordAndCodeBlocks/MainChallenge.java
+++ IfKeywordAndCodeBlocks/MainChallenge.java
@@ -1,19 +1,32 @@
-package IfKeywordAndCodeBlocks;
-
 public class MainChallenge {
+
     public static void main(String[] args) {
+
         boolean gameOver = true;
         int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
 
-       int finalScore = score;
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("The highScore is " + highScore);
 
-       if (gameOver == true){
-           finalScore += (levelCompleted * bonus);
-           System.out.println("Your final score was " + finalScore);
-       }
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
 
+        System.out.println("The next highScore is " +
+                calculateScore(gameOver, score, levelCompleted, bonus));
+    }
 
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+        }
+
+        return finalScore;
     }
 }
