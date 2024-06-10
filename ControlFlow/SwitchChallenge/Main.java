95f309b7930ae1bf37f507d1748441301492889c
Gav Shields <65550958+Nizzer01@users.noreply.github.com>
2024-06-10 08:52:32 +0100

    Control Flow overview

---
+++ ControlFlow/SwitchChallenge/Main.java
@@ -0,0 +1,29 @@
+package ControlFlow.SwitchChallenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char charValue = 'X';
+        switch (charValue) {
+            case 'A':
+                System.out.println("A is alpha");
+                break;
+            case 'B':
+                System.out.println("B is bravo");
+                break;
+            case 'C':
+                System.out.println("C is charlie");
+                break;
+            case 'D':
+                System.out.println("D is delta");
+                break;
+            case 'E':
+                System.out.println("E is echo");
+                break;
+            default:
+                System.out.println("Letter " + charValue + " was not found in the switch");
+                break;
+        }
+    }
+}
